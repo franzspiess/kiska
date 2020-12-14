0e3b4e4bc6d05148eed2032224a4309972867343
franzspiess <franz.spiess@gmail.com>
2020-12-14 20:26:28 +0100

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import logo from './kiska.jpg';
+// @ts-ignore
+import logo from './kiska.JPG'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      </div>
     </div>
   );
 }
